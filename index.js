658cab77e3f992742a71fca1af50de78ea05af20
ankit choudhary <ankitc98147@gmail.com>
2023-10-16 19:51:08 +0530

    all initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+
+//  const loadScript = (src) => {
+//     return new Promise((resolve, reject) => {
+//         let script = document.createElement("script");
+//         script.type = "text/javascript";
+//         script.src = src;
+//         document.head.appendChild(script);
+//         script.onload = () => {
+//             resolve("Script is loaded.");
+//         }
+//         script.onerror = () => {
+//             reject("Error to load script.");
+//         }    
+
+//     })
+// }
+// let script = loadScript("https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js");
+// script.then((value) => {
+//     console.log(value);
+// }).catch((error) => {
+//     console.log("We are sorry but we are having issue to loading this script.");    
+// })
+//         var typed = new Typed('#heading', {
+//             strings: [`As a web designer, my objective is to make a positive impact on clients, co-workers, and the Internet using my skills and experience to design compelling and attractive websites`],
+//             typeSpeed: 50,
+//         });
+   
+       
